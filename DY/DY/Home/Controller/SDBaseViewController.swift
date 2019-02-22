b282c9cc0a34d59db8ac36d9429d08490212791a
王丹 <1653591454@qq.com>
2019-02-22 11:33:17 +0800

    初始化项目

---
+++ DY/DY/Home/Controller/SDBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SDBaseViewController.swift
+//  DY
+//
+//  Created by sh-lx on 2019/2/22.
+//  Copyright © 2019年 WangDan. All rights reserved.
+//
+
+import UIKit
+
+class SDBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
